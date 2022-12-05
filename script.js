f0a025260bfd6ca24ee5e82a8dd65c335499ab86
Onegreat <wangreat@yandex.ru>
2022-12-05 21:01:35 +0300

    Реализовал вывод данных через массив

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let lang = prompt("Выберете язык: (ru или en)");
+let lang_array = [];
+
+lang_array['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+lang_array['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
 
 if (lang == "ru") {
     console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
@@ -17,5 +23,6 @@ switch (lang) {
         break
     default:
         console.log("Допущена ошибка в выборе языка")
+}
 
-}       
+console.log(lang_array[lang]);
