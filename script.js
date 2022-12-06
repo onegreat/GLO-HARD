adaa52f53daff9f554fdc9ac4a940cdd51e8b5f0
Onegreat <wangreat@yandex.ru>
2022-12-06 21:45:58 +0300

    Реализовал работу функции по удалению пробелом и сокращение символов до 30

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-'use strick'
 
-const prsonalHelp(str) {
+'use strict'
+
+const userHelp = function (str) {
     if (typeof str === 'string') {
+        str = str.trim()
+        if (str.length > 30) return `${str.substr(0, 30)}...`
         return str
     }
     return `${str} - не строка`
 }
-console.log()
+
+
+console.log(userHelp(32324))
+console.log(userHelp(' 43434 '))
+console.log(userHelp("23333333333333333333333333333333333333333333333"))
