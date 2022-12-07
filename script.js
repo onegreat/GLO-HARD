4d5c8952878d0341a0c27d4d268b2ef0c289bc70
Onegreat <wangreat@yandex.ru>
2022-12-07 21:20:03 +0300

    Реализовал массив

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for (let i = 0; i < arr.length; i++) {
         console.log(arr[i])
     }
 }
+
+function isPrime(n) {
+    for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
+    return n > 1;
+}
+
+const res = [...Array(100)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
+
+console.log(res);
