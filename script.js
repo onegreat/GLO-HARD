91beba132bfaa6f933ee3e269a9316a45dd27409
Onegreat <wangreat@yandex.ru>
2022-12-12 19:18:09 +0300

    Реализовал задачу с массивом

---
+++ script.js
@@ -0,0 +1,7 @@
+let days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "<i>Суббота</i>", "<i>Воскресенье</i>"];
+
+let today_index = (6 + new Date().getDay()) % 7;
+days[today_index] = `<b>${days[today_index]}</b>`;
+
+document.body.insertAdjacentHTML("beforeEnd", days.join("<br>"));
+
